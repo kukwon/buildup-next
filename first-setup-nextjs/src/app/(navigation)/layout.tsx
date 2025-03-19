612a4fe407f846311a8a51a9435959e4d6c30fa1
kukwon <rnrdnjs415@daum.net>
2025-03-19 19:39:43 +0900

    9-1 Left Navigation Bar(LNB) 추가 - <a> anchor 태그 활용

--- first-setup-nextjs/src/app/(navigation)/layout.tsx
+++ first-setup-nextjs/src/app/(navigation)/layout.tsx
@@ -1,3 +1,14 @@
+function LinkButton({ children, href }) {
+  return (
+    <a
+      href={href}
+      className="border-2 border-solid p-[10px] border-white block"
+    >
+      {children}
+    </a>
+  );
+}
+
 export default function NavigationLayout({
   children,
 }: Readonly<{
@@ -5,10 +16,20 @@ export default function NavigationLayout({
 }>) {
   return (
     <>
-      <div className="w-full bg-red-700">
-        모든 (navigation) 페이지에서의 공통영역 RootLayout
+      <div className="flex flex-row">
+        <div className="w-full md:w-44">
+          <LinkButton href="/">Logo</LinkButton>
+          <LinkButton href="/dashboard">1. Dashboard</LinkButton>
+          <LinkButton href="/dashboard/invoices">2. Invoices</LinkButton>
+          <LinkButton href="/dashboard/customers">3. Customers</LinkButton>
+        </div>
+        <div className="flex-grow">
+          <div className="w-full bg-red-700">
+            모든 (navigation) 페이지에서의 공통영역 RootLayout
+          </div>
+          {children}
+        </div>
       </div>
-      {children}
     </>
   );
 }
