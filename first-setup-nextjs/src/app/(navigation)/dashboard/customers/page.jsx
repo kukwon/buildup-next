b99d4aa16b6e3b84c2a3d529b1118252bbca8545
kukwon <rnrdnjs415@daum.net>
2025-03-19 18:08:29 +0900

    7-1 페이지 조회 시 throw new Error 통한 에러 발생상황 재현

--- first-setup-nextjs/src/app/(navigation)/dashboard/customers/page.jsx
+++ first-setup-nextjs/src/app/(navigation)/dashboard/customers/page.jsx
@@ -1,4 +1,7 @@
 export default function Customer() {
+  if (true) {
+    throw new Error("일부러 에러 발생");
+  }
   return (
     <main className="text-center bg-blue-100 p-6 border-b-8 border-blue-700">
       <div className="text-blue-500">Customer 페이지</div>
