ba6517247ca8b66ee4dfbd930ee62ce47de84de4
kukwon <rnrdnjs415@daum.net>
2025-03-19 18:50:52 +0900

    8-1 페이지 조회 시 로딩이 오래걸리는 상황 재현

--- first-setup-nextjs/src/app/(navigation)/dashboard/customers/page.jsx
+++ first-setup-nextjs/src/app/(navigation)/dashboard/customers/page.jsx
@@ -1,7 +1,7 @@
-export default function Customer() {
-  if (true) {
-    throw new Error("일부러 에러 발생");
-  }
+const wait = (time) => new Promise((resolve) => setTimeout(resolve, time));
+export default async function Customer() {
+  await wait(3000);
+
   return (
     <main className="text-center bg-blue-100 p-6 border-b-8 border-blue-700">
       <div className="text-blue-500">Customer 페이지</div>
