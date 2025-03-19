711b42822da79bcf36e2cd15860956ed77300ec4
kukwon <rnrdnjs415@daum.net>
2025-03-20 00:29:12 +0900

    13-2 Single Segment - [slug] 단일 세그먼트 인입하기 위한 버튼 추가

--- first-setup-nextjs/src/app/(navigation)/dashboard/customers/page.jsx
+++ first-setup-nextjs/src/app/(navigation)/dashboard/customers/page.jsx
@@ -1,3 +1,4 @@
+import ServerLinkButton from "@/components/ServerLinkButton";
 const wait = (time) => new Promise((resolve) => setTimeout(resolve, time));
 export default async function Customer() {
   await wait(3000);
@@ -5,6 +6,9 @@ export default async function Customer() {
   return (
     <main className="text-center bg-blue-100 p-6 border-b-8 border-blue-700">
       <div className="text-blue-500">Customer 페이지</div>
+      <ServerLinkButton href="customers/aaron">Aaron 고객</ServerLinkButton>
+      <ServerLinkButton href="customers/baron">Baron 고객</ServerLinkButton>
+      <ServerLinkButton href="customers/caron">Caron 고객</ServerLinkButton>
     </main>
   );
 }
