2a88497f01f41201ad7f89c1969e1e54fe3f0e99
kukwon <rnrdnjs415@daum.net>
2025-03-21 16:58:23 +0900

    15-2 Catch-all Segment - [...slug] 다수 세그먼트 인입하기 위한 버튼 추가

--- first-setup-nextjs/src/app/(navigation)/dashboard/invoices/page.jsx
+++ first-setup-nextjs/src/app/(navigation)/dashboard/invoices/page.jsx
@@ -1,7 +1,19 @@
+import ServerLinkButton from "@/components/ServerLinkButton";
+
 export default function Invoice() {
   return (
     <main className="text-center bg-green-100 p-6 border-b-8 border-green-700">
       <div className="text-green-500">Invoice 페이지</div>
+
+      <ServerLinkButton href="invoices/fix/computer">
+        컴퓨터 고쳐주세요
+      </ServerLinkButton>
+      <ServerLinkButton href="invoices/buy/bread">
+        빵 사다주세요
+      </ServerLinkButton>
+      <ServerLinkButton href="invoices/pay/apple">
+        사과 결제해주세요
+      </ServerLinkButton>
     </main>
   );
 }
